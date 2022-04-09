7e9056aeaaffcfedb8982a02791070df56218601
fosterea <foster.angus@gmail.com>
2022-04-09 12:56:01 -0700

    poop

--- font.js
+++ font.js
@@ -1,20 +1,20 @@
-// Changes the css to customize for phones
+// // Changes the css to customize for phones
 
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 
-    if (navigator.userAgent.match(/Android/i) //I copied this if statement from https://redstapler.co/detect-mobile-device-with-javascript/
-    || navigator.userAgent.match(/webOS/i) //This if statment is true if the website is being run in a mobile browser
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)) {
-    //Sets the font-size of the page if on mobile
-    let r = document.querySelector(':root');
+//     if (navigator.userAgent.match(/Android/i) //I copied this if statement from https://redstapler.co/detect-mobile-device-with-javascript/
+//     || navigator.userAgent.match(/webOS/i) //This if statment is true if the website is being run in a mobile browser
+//     || navigator.userAgent.match(/iPhone/i)
+//     || navigator.userAgent.match(/iPad/i)
+//     || navigator.userAgent.match(/iPod/i)
+//     || navigator.userAgent.match(/BlackBerry/i)
+//     || navigator.userAgent.match(/Windows Phone/i)) {
+//     //Sets the font-size of the page if on mobile
+//     let r = document.querySelector(':root');
 
-    r.style.setProperty('--skillWidth', '25%');
-  } else {
-    //Sets the font-size of the page if not on mobile
+//     r.style.setProperty('--skillWidth', '25%');
+//   } else {
+//     //Sets the font-size of the page if not on mobile
     
-  }
-});
+//   }
+// });
